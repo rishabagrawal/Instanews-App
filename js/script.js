b055e0d107a2f1527e1fda2ac0bc69d7751cc867
Rishab <rishabagrawal@hotmail.com>
2019-05-02 11:11:04 -0700

    add code for changed method, appended abstract and image, in css add color so text shows up, in html add div and p

--- js/script.js
+++ js/script.js
@@ -1,8 +1,26 @@
-const section='science'
+
+$('#selection-bar').on('change', function(){
+    const section = $(this).val();
+    if (section !== '')
+ 
 $.ajax({
     method:'get',
-    url:'https://api.nytimes.com/svc/topstories/v2/'+section+'.json?api-key=GEF9XRHhNY0ZEEyDvzTckQt7YaVziB1d'
+    url:'https://api.nytimes.com/svc/topstories/v2/'+section+'.json?api-key=GEF9XRHhNY0ZEEyDvzTckQt7YaVziB1d'    
+    // url:'https://api.nytimes.com/svc/topstories/v2/'+section+'.json?api-key=GEF9XRHhNY0ZEEyDvzTckQt7YaVziB1d'
 })
 .done(function(data){
     console.log(data);
+    $(".stories").html("");
+    $.each(data.results, function(key, value){
+        const text = value.abstract;
+        const pic = value.multimedia[4].url;
+        $(".stories").append(`
+            <p>${text}</p>
+        `);
+        $(".stories").append(`
+        <img src= ${pic} >`);
+
+    })
+})   
 });
+
